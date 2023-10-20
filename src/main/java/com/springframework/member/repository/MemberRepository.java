c1d438a74dfde3b0fbf42d07ec4c8a24a7f836db
Yoonju Ahn <105343169+raagdol@users.noreply.github.com>
2023-10-20 17:07:25 +0900

    회원가입 데이터 db확인

--- src/main/java/com/springframework/member/repository/MemberRepository.java
+++ src/main/java/com/springframework/member/repository/MemberRepository.java
@@ -2,13 +2,15 @@ package com.springframework.member.repository;
 
 import com.springframework.member.dto.MemberDTO;
 import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
+	private final SqlSessionTemplate sql;
 	public int save(MemberDTO memberDTO) {
 		System.out.println("memberDTO = " + memberDTO);
-		return 0;
+		return sql.insert("Member.save", memberDTO);
 	}
 }
